fc138f4a6918ec95a9c63dd015fc4428a0af9619
SHREYA NARAYAN <140142783+shreyyeah@users.noreply.github.com>
2023-10-27 22:39:11 +0530

    Add files via upload

---
+++ length of string.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,len;
+    char name[3456];
+    cout<< "enter name"; //input name 
+    cin>> name;
+    cout<<"you entered"<< name <<endl; //print the value entered
+    while(name [i != '\0'])
+{
+    len++;
+    i++;
+}
+cout<<"length of name is"<<len; //print length
+}
